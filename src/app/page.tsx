02f5f59b6702b98b24c614bad07e0ce1a76fd153
Luiz Chaves <luiz.chaves@hotmart.com>
2024-07-23 00:51:53 -0300

    feat: improve red color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex w-full h-screen items-center justify-center bg-red-900">
+    <main className="flex w-full h-screen items-center justify-center bg-red-400">
       <h1 className="text-6xl text-center text-white font-extrabold">
         My Ifood!
       </h1>
